abc83a9ecf947f99b607a5415fd88b5610a0181e
vadimcvirko <promiti@yandex.ru>
2021-03-09 13:16:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0 
+  }
+
+  let minTotal = array.reduce((accumulator , currentValue) => {
+    if (accumulator < currentValue ){
+      return accumulator
+    } else {
+      return currentValue
+    }
+  },0);
+  return minTotal ;
 }
 
+
+
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined ||  array.length === 0){
+    return 0 
+  }
+  let maxTotal = array.reduce((accumulator,currentValue ) => {
+    if (accumulator > currentValue){
+      return accumulator
+    } else {
+      return currentValue
+    }
+
+  })
+
+
+  return maxTotal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined ||  array.length === 0){
+    return 0 
+  }
+  const  total = array.reduce((total,number) => {
+    return total + number;
+  })
+  return total / array.length;
 }
